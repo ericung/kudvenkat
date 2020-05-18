1fad3b84d7105e729f07c7e7bd0b1b513a5b699a
Eric Ung <ungericw@tutanota.com>
2020-05-18 15:40:52 -0500

    5/18/2020 UseDefaultFiles

--- kudvenkat/Startup.cs
+++ kudvenkat/Startup.cs
@@ -29,7 +29,7 @@ namespace kudvenkat
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
       if (env.IsDevelopment())
       {
@@ -51,24 +51,15 @@ namespace kudvenkat
       });
       */
 
-      app.Use(async (context, next) =>
-      {
-        logger.LogInformation("MW1: Incoming Request");
-        await next();
-        logger.LogInformation("MW1: Outgoing Response");
-      });
-
-      app.Use(async (context, next) =>
-      {
-        logger.LogInformation("MW2: Incoming Request");
-        await next();
-        logger.LogInformation("MW2: Outgoing Response");
-      });
+      DefaultFilesOptions defaultFilesOptions = new DefaultFilesOptions();
+      defaultFilesOptions.DefaultFileNames.Clear();
+      defaultFilesOptions.DefaultFileNames.Add("foo.html");
+      app.UseDefaultFiles(defaultFilesOptions);
+      app.UseStaticFiles();
 
       app.Run(async (context) =>
       {
-        await context.Response.WriteAsync("Hello from third middleware.");
-        logger.LogInformation("MW3: Request handled");
+        await context.Response.WriteAsync("Hello world!");
       });
 
     }
