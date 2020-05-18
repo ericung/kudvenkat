ca0a475876f7d5802d3e61d3552f25ab55c28fe8
Eric Ung <ungericw@tutanota.com>
2020-05-18 15:18:41 -0500

    5/18/2020 middleware

--- kudvenkat/Startup.cs
+++ kudvenkat/Startup.cs
@@ -5,13 +5,23 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace kudvenkat
 {
   public class Startup
   {
+    private IConfiguration _config;
+
+    public Startup(IConfiguration config)
+    {
+      _config = config;
+    }
+
+
     // This method gets called by the runtime. Use this method to add services to the container.
     // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     public void ConfigureServices(IServiceCollection services)
@@ -19,22 +29,48 @@ namespace kudvenkat
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
     {
       if (env.IsDevelopment())
       {
         app.UseDeveloperExceptionPage();
       }
 
+      /*
       app.UseRouting();
 
+      
       app.UseEndpoints(endpoints =>
       {
         endpoints.MapGet("/", async context =>
               {
-            await context.Response.WriteAsync("Hello World!");
+            // await context.Response.WriteAsync(System.Diagnostics.Process.GetCurrentProcess().ProcessName);
+            await context.Response.WriteAsync(_config["MyKey"]);
           });
+
       });
+      */
+
+      app.Use(async (context, next) =>
+      {
+        logger.LogInformation("MW1: Incoming Request");
+        await next();
+        logger.LogInformation("MW1: Outgoing Response");
+      });
+
+      app.Use(async (context, next) =>
+      {
+        logger.LogInformation("MW2: Incoming Request");
+        await next();
+        logger.LogInformation("MW2: Outgoing Response");
+      });
+
+      app.Run(async (context) =>
+      {
+        await context.Response.WriteAsync("Hello from third middleware.");
+        logger.LogInformation("MW3: Request handled");
+      });
+
     }
   }
 }
