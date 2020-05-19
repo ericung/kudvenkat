c38f125d1f98ba4e7b087762ca1d8d982d394530
Eric Ung <ungericw@tutanota.com>
2020-05-19 14:41:11 -0500

    5/19/2020 dependency injection tutorial

--- kudvenkat/Controllers/HomeController.cs
+++ kudvenkat/Controllers/HomeController.cs
@@ -1,15 +1,24 @@
-﻿using System;
+﻿using kudvenkat.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace kudvenkat.Controllers
 {
-  public class HomeController
+  public class HomeController : Controller
   {
+    private IEmployeeRepository _employeeRepository;
+
+    public HomeController(IEmployeeRepository employeeRepository)
+    {
+      _employeeRepository = employeeRepository;
+    }
+
     public string Index()
     {
-      return "Hello from MVC";
+      return _employeeRepository.GetEmployee(1).Name;
     }
   }
 }
--- kudvenkat/Startup.cs
+++ kudvenkat/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using kudvenkat.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -28,6 +29,7 @@ namespace kudvenkat
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddMvc();
+      services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
