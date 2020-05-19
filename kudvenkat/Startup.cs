e656a4290ac18495304fdfd217b5fb367aa01a0b
Eric Ung <ungericw@tutanota.com>
2020-05-19 12:22:17 -0500

    5/19/2020 add mvc

--- kudvenkat/Startup.cs
+++ kudvenkat/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +27,7 @@ namespace kudvenkat
     // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     public void ConfigureServices(IServiceCollection services)
     {
+      services.AddMvc();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,11 +40,15 @@ namespace kudvenkat
 
       app.UseStaticFiles();
 
-      app.Run(async (context) =>
-      {
-        await context.Response.WriteAsync("Hosting environment: " + env.EnvironmentName);
-      });
+      // 3.0 modification
+      // https://stackoverflow.com/questions/57684093/using-usemvc-to-configure-mvc-is-not-supported-while-using-endpoint-routing
+      app.UseRouting();
+      app.UseCors();
 
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}");
+      });
     }
   }
 }
