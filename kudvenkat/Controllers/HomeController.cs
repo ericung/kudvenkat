7b7ac6c4f6d1e0f36993fa7595acb9cf7f7d6be0
Eric Ung <ungericw@tutanota.com>
2020-06-05 17:17:59 -0500

    6/5/2020 working upload

--- kudvenkat/Controllers/HomeController.cs
+++ kudvenkat/Controllers/HomeController.cs
@@ -56,9 +56,16 @@ namespace kudvenkat.Controllers
         string uniqueFileName = null;
         if (model.Photo != null)
         {
+          // The image must be uploaded to the images folder in wwwroot
+          // To get the path of the wwwroot folder we are using the inject
+          // HostingEnvironment service provided by ASP.NET Core
           string uploadsFolder = Path.Combine(_hostingEnvironment.WebRootPath, "images");
-          uniqueFileName = Guid.NewGuid().ToString() + "_" + model.Photo.FileName;
+          // To make sure the file name is unique we are appending a new
+          // GUID value and and an underscore to the file name
+          uniqueFileName = Guid.NewGuid().ToString() + "_" + Path.GetFileName(model.Photo.FileName);
           string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+          // Use CopyTo() method provided by IFormFile interface to
+          // copy the file to wwwroot/images folder
           model.Photo.CopyTo(new FileStream(filePath, FileMode.Create));
         }
 
@@ -71,7 +78,7 @@ namespace kudvenkat.Controllers
         };
 
         _employeeRepository.Add(newEmployee);
-        RedirectToAction("details", new { id = newEmployee.Id });
+        return RedirectToAction("details", "home", new { id = newEmployee.Id });
       }
 
       return View();
